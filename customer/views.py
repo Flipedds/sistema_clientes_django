7d1d1b42532159463f88847a66ab1682245d174e
Filipe André <flipeandre12@gmail.com>
2023-02-23 06:31:50 -0300

    Initial commit

---
+++ customer/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+# Create your views here.
+from django.views.generic import ListView, CreateView
+from .models import Customer
+from .forms import CustomerForm
+from django.urls import reverse
+
+
+class CustomerlistView(ListView):
+    template_name = "customer/customer_list.html"
+    model = Customer
+    queryset = Customer.objects.all()
+
+
+class CustomerCreateView(CreateView):
+    template_name = "customer/customer.html"
+    form_class = CustomerForm
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("customer:customer-list")
